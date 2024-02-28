52792c73a197f2da9d1e07b2cc9de723516eab05
cloud-network-guy <greenlakejohnny@gmail.com>
2024-02-28 14:19:14 -0800

    downgrade random version

---
+++ versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.3.4"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.49.0, < 6.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.1.0"
+    }
+  }
+}
